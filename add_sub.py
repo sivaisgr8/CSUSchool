ad52976c5728a6d21b5bfa80fb31f858d86d5830
sivaisgr8 <sriram.kandasamy@csuglobal.edu>
2026-01-18 04:59:47 -0500

    Intial file

---
+++ add_sub.py
@@ -0,0 +1,27 @@
+import sys
+import io
+import os
+
+def mathfunction ():
+    #mathfunction1='n'
+    firstNumber = int(input('Please enter first number: '))
+    secondNumber = int(input('Please enter second number: '))
+
+    while True:
+        operation = input('Do you want to add (y ) or subtract(n): ')
+        if operation == 'y':
+           print(f"Result: {firstNumber + secondNumber}")
+           break
+        elif operation == 'n':
+            print(f"Result: {firstNumber - secondNumber}")
+            break
+        else:
+            print('Invalid Selection. Please enter y or n only')
+
+
+mathfunction ()
+
+
+
+
+
