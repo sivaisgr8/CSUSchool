627ba09e4a799b94de8e85562eb2555c24e6adca
sivaisgr8 <sriram.kandasamy@csuglobal.edu>
2026-01-18 05:10:57 -0500

    Intial file

--- mul_div.py
+++ mul_div.py
@@ -8,24 +8,19 @@ def mathfunction ():
     secondNumber = int(input('Please enter second number: '))
 
     while True:
-        operation = input('Do you want to add (a ) or subtract(s) or both(b): ')
+        operation = input('Do you want to multiply (m ) or divide(d) or both(b): ')
         if operation == 'y':
-           print(f"Result: {firstNumber + secondNumber}")
+           print(f"Result: {firstNumber * secondNumber}")
            break
         elif operation == 'n':
-            print(f"Result: {firstNumber - secondNumber}")
+            print(f"Result: {firstNumber / secondNumber}")
             break
         elif operation == 'b':
-            print(f"Result Add: {firstNumber + secondNumber}")
-            print(f"Result Add: {firstNumber - secondNumber}")
+            print(f"Result Multiplication: {firstNumber * secondNumber}")
+            print(f"Result Division: {firstNumber / secondNumber}")
             break
         else:
             print('Invalid Selection. Please enter y or n only')
 
 
 mathfunction ()
-
-
-
-
-
